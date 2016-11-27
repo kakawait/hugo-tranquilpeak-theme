c27f531c3bbc4a96e3b008c71c3ef97da6dbc74a
Thibaud Leprêtre <thibaud.lepretre@gmail.com>
2016-11-27 11:12:48 +0100

    De-bundlize external script
    For many reasons:
    
    1. Easier to upgrade for user, simply change html
    2. Avoid growing git repo size
    3. Allow browser cache usage
    
    Upgrade fontawesome in same time that fixes #58

--- tasks/register/build.js
+++ tasks/register/build.js
@@ -2,10 +2,8 @@ module.exports = function(grunt) {
   // Build (environment : development)
   grunt.registerTask('build', [
     'clean:build',
-    'bower:dev',
     'syncAssets',
     'linkAssets',
-    'replace:cssFancybox',
     'replace:cssTranquilpeak'
   ]);
 };
