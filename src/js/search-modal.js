6ba465d4d422cfeabd39756b92748416c32a9d2f
tobiasmcvey <tobias.mcvey@gmail.com>
2020-05-02 20:16:50 +0200

    trying open sans font (#2)
    * trying open sans font
    
    * Delete 404.html
    
    * Delete index.html
    
    * Delete index.xml
    
    * Delete style-twzjdbqhmnnacqs0pwwdzcdbt8yhv8giawvjqjmyfoqnvazl0dalmnhdkvp7.min.css
    
    * Delete index.html
    
    * Delete index.xml
    
    * Delete index.html
    
    * Delete sitemap.xml
    
    * Delete index.html
    
    * Delete index.xml

--- src/js/search-modal.js
+++ src/js/search-modal.js
@@ -135,7 +135,7 @@
         html += moment(post.date).locale(lang).format('ll');
         html += '</span>';
         html += '</span>';
-        html += '<div class="media-content hide-xs font-merryweather">' + post.excerpt + '</div>';
+        html += '<div class="media-content hide-xs font-open-sans-serif">' + post.excerpt + '</div>';
         html += '</div>';
         html += '<div style="clear:both;"></div>';
         html += '<hr>';
