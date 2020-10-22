a58aad83c9314d9d5a31999bfadf037fa3e48e67
Anceret Matthieu <anceret.matthieu@gmail.com>
2020-10-22 23:25:36 +0200

    Intégration du dark mode (#1)
    * Création d'un composant toggle permet de basculer entre les thèmes dark/light
    * Intégration du toggle dans la sidebar
    * Masquage de la modal "About me" via la touche échap
    * Ajustement du thème pour gérer les composants en mode dark
    * Ajustements sur les pages archives/tags/catégories et sur les top/bottom bars d'un article
    * Réduction de la luminosité des images en mode dark

---
+++ src/js/toggle.js
@@ -0,0 +1,22 @@
+﻿function toggleTheme() {
+    if (localStorage.getItem('theme') === 'dark') {
+        setTheme('light');
+    } else {
+        setTheme('dark');
+    }     
+}
+
+(function($) {
+    'use strict';
+
+    $(document).ready(function() {
+        var currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+        if (currentTheme) {
+            if (currentTheme === 'dark') {
+                document.getElementById('slider').checked = false;
+            } else {
+                document.getElementById('slider').checked = true;
+            }
+        }
+    });      
+})(jQuery);
